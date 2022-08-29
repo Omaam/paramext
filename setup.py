87972f628aa79defe3edf03635bfb0fa4d4bf84e
Omaam <omama.tomoki@gmail.com>
2022-08-29 13:53:57 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pex',
+    version='0.0.1',
+    description="Parameter Extractor for Xspec",
+    long_description="",
+    author='Tomoki Omama',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    packages=['pex']
+)
